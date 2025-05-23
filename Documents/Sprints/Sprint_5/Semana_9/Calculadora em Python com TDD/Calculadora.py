40dc76c14130351d47f4bed1f03f3d043522c8e2
Gvitor10 <vitorgabriel7062@outlook.com>
2025-05-23 13:59:49 -0300

    Backup Repository

---
+++ Documents/Sprints/Sprint_5/Semana_9/Calculadora em Python com TDD/Calculadora.py
@@ -0,0 +1,16 @@
+class Calculadora:
+    @staticmethod
+    def somar(num1, num2):
+        return num1 + num2
+
+    @staticmethod
+    def subtrair(num1, num2):
+        return num1 - num2
+
+    @staticmethod
+    def multiplicar(num1, num2):
+        return num1 * num2
+
+    @staticmethod
+    def dividir(num1, num2):
+        return num1 / num2
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pb.compassouol
+# pb.compassuol
+Acompanhe a minha jornada como um QA júnior. Aqui será postada toda a minha evolução, artefatos e materiais produzidos ao longo da minha jornada!
